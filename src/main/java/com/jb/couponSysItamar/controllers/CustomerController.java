e3131380c389bf87eb80a46a7b22421d14e3105b
Itamar <itamarshr@pelch.ort.org.il.com>
2023-02-18 23:36:23 +0200

    My first commit

--- src/main/java/com/jb/couponSysItamar/controllers/CustomerController.java
+++ src/main/java/com/jb/couponSysItamar/controllers/CustomerController.java
@@ -40,13 +40,6 @@ public class CustomerController {
         customerService.purchaseCoupon(token, couponId);
     }
 
-//    @GetMapping("{customerId}/coupons")
-//    public List<Coupon> getAllCustomerPurchasedCoupons(@PathVariable int customerId, @RequestHeader("Authorization") UUID token) throws CouponSystemException {
-//        if (!tokenService.isValid(token, ClientType.CUSTOMER)) {
-//            throw new CouponSystemException(ErrMsg.INVALID_TOKEN);
-//        }
-//        return customerService.getAllCustomerPurchasedCoupons(customerId);
-//    }
 
     @GetMapping("/token/{token}/coupons")
     public List<Coupon> getAllCustomerPurchasedCoupons(@PathVariable UUID token, @RequestHeader("Authorization") UUID token2) throws CouponSystemException {
@@ -56,20 +49,5 @@ public class CustomerController {
         return customerService.getAllCustomerPurchasedCouponsByToken(token);
     }
 
-//    @GetMapping("{customerId}/coupons/category")
-//    public List<Coupon> getAllCustomerPurchasedCouponsByCategory(@PathVariable int customerId, @RequestParam String category) throws CouponSystemException {
-//
-//        return customerService.getAllCustomerPurchasedCouponsByCategory(customerId, Category.valueOf(category));
-//    }
-//
-//    @GetMapping("{customerId}/coupons/maxPrice")
-//    public List<Coupon> getAllCustomerPurchasedCouponsByMaxPrice(@PathVariable int customerId, @RequestParam double maxPrice) throws CouponSystemException {
-//        return customerService.getAllCustomerPurchasedCouponsByMaxPrice(customerId, maxPrice);
-//    }
-//
-//    @GetMapping("{customerId}")
-//    public Customer getLoginCustomer(@PathVariable int customerId) throws CouponSystemException {
-//        return customerService.getLoginCustomer(customerId);
-//    }
 
 }
